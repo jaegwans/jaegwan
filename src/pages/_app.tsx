65b67e9a655e792f8c87f0f2f21a86d53197b2b7
jaegwans <skhu201811140@gmail.com>
2023-08-16 10:20:23 +0900

    feat: head viewport

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,13 @@
 import type { AppProps } from 'next/app';
 import GlobalStyle from '../styles/globalStyle';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
         <>
+            <Head>
+                <meta name="viewport" content="viewport-fit=cover" />
+            </Head>
             <GlobalStyle />
             <Component {...pageProps} />
         </>
