b994bf1ccee6a6651982536af998a354cd976a01
jaegwans <skhu201811140@gmail.com>
2023-08-16 10:24:07 +0900

    fix: viewport

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,7 +6,10 @@ export default function App({ Component, pageProps }: AppProps) {
     return (
         <>
             <Head>
-                <meta name="viewport" content="viewport-fit=cover" />
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1, maximum-scale=1"
+                />
             </Head>
             <GlobalStyle />
             <Component {...pageProps} />
