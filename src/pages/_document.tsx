eb691ee8849cf4d916a25dcbb4673b0b66f88db5
jaegwans <skhu201811140@gmail.com>
2023-08-16 09:31:18 +0900

    feat: main

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,34 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import Document from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+export default class MyDocument extends Document {
+    static async getInitialProps(ctx: any) {
+        const sheet = new ServerStyleSheet();
+        const originalRenderPage = ctx.renderPage;
+
+        try {
+            // sheet을 사용해 정의된 모든 스타일을 수집
+            ctx.renderPage = () =>
+                originalRenderPage({
+                    enhanceApp: (App: any) => (props: any) =>
+                        sheet.collectStyles(<App {...props} />),
+                });
+
+            // Documents의 initial props
+            const initialProps = await Document.getInitialProps(ctx);
+
+            // props와 styles를 반환
+            return {
+                ...initialProps,
+                styles: (
+                    <>
+                        {initialProps.styles}
+                        {sheet.getStyleElement()}
+                    </>
+                ),
+            };
+        } finally {
+            sheet.seal();
+        }
+    }
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,9 +3,40 @@ import Image from 'next/image';
 import styled from 'styled-components';
 
 function Home() {
-    return <StyledHome>hello</StyledHome>;
+    return (
+        <StyledHome>
+            <h2>jaegwan.com</h2>
+            <StyledLink href="https://blog.jaegwan.com">blog</StyledLink>
+            <StyledLink href="https://github.com/jaegwans">github</StyledLink>
+            <StyledLink href="https://docs.google.com/document/d/1D5oPFkMqpe1dYCPoW6LAHp9gBBHgHQtdWg11ADhVP1A/edit?usp=sharing">
+                resume
+            </StyledLink>
+        </StyledHome>
+    );
 }
 
 export default Home;
 
-const StyledHome = styled.div``;
+const StyledHome = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: flex-start;
+    gap: 3rem;
+    height: 100vh;
+    margin-top: 3rem;
+    a {
+        text-decoration: none;
+    }
+`;
+
+const StyledLink = styled.a`
+    color: #5a5a5a;
+    font-size: 2rem;
+
+    transition: transform 0.3s ease-in-out, color 0.3s ease-in-out;
+    &:hover {
+        color: #000000;
+        transform: translateY(-0.5rem);
+    }
+`;
